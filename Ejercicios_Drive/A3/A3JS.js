8fdb55154d2a50ad1b578640bd668cc65a03515f
mrtnmmn <user.email>
2021-10-06 14:48:38 +0200

    add ej A3

--- Ejercicios_Drive/A3/A3JS.js
+++ Ejercicios_Drive/A3/A3JS.js
@@ -1,3 +1,23 @@
 function validate() {
-    
+    let regex1 = /^(?:0?[1-9]\d{3}|[1-4]\d{4}|5[0-2]\d{3})$/;
+    let val = document.getElementById("txt").value
+    let result = ''
+
+    if (regex1.test(val)) {
+        result += '<p>Es un codigo postal</p>'
+    } else {
+        result += '<p>No es un codigo postal</p>'
+    }
+
+    result += '<p>' + val + '&nbsp' + val + '&nbsp' + val + '</p>'
+
+    let regex2 = /^Sa\w*do$/
+
+    if (regex2.test(val)) {
+        result += '<p>Empieza por \'Sa\' y acaba por \'do\'</p><br>'
+    } else {
+        result += '<p>No empieza por \'Sa\' y acaba por \'do\'</p><br>'    
+    }
+
+    document.getElementById("result").innerHTML = result
 }
