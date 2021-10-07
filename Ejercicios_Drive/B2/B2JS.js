f6d5a56665e8262b0ee0e3019e4cc3ccafe4b76d
mrtnmmn <user.email>
2021-10-07 09:05:14 +0200

    added indexes

---
+++ Ejercicios_Drive/B2/B2JS.js
@@ -0,0 +1,18 @@
+
+let date = {
+    day: getDate(),
+    month: getmonth(),
+    year: getYear()
+} 
+let text = date.day = '/' + date.month + '/' + date.year
+
+console.log(text)
+
+if (date.month === 1 || date.month === 12) {
+    if (month === 1 && (date.day > 1 || date.day < 10)) {
+
+    }
+    if (month === 12) {
+        
+    }
+}
